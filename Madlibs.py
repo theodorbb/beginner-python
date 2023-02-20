560dd80502d83e1b61c43c8ada560307d8a26886
theodorbb <111547477+theodorbb@users.noreply.github.com>
2023-02-20 20:03:00 +0200

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,5 @@
+adj = input("Adjective: ")
+verb1=input("Verb: ")
+name=input("Name: ")
+madlib=f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1} with {name}!"
+print(madlib)
